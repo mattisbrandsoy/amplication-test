ae71008375c6ff1d73a5f27848e0781cadee51b9
Mattis <mattis.brandsoy@icloud.com>
2022-04-05 16:04:48 +0200

    Re added custom auth endpoints

--- server/src/auth/token.service.ts
+++ server/src/auth/token.service.ts
@@ -1,6 +1,11 @@
-import { ITokenService } from "./ITokenService";
+import { ITokenService } from './ITokenService';
 //@ts-ignore
 // eslint-disable-next-line import/no-unresolved
-import { TokenServiceBase } from "./base/token.service.base";
+import { TokenServiceBase } from './base/token.service.base';
 //@ts-ignore
-export class TokenService extends TokenServiceBase implements ITokenService {}
+export class TokenService extends TokenServiceBase implements ITokenService {
+	// return username from jwt token
+	decodeToken(bearer: string): string {
+		return this.jwtService.verify(bearer).username;
+	}
+}
