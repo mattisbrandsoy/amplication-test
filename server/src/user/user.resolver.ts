6f54e25ac2d9ccccda4a0e3f36b6443e8852a6e8
Mattis <mattis.brandsoy@icloud.com>
2022-04-05 23:34:01 +0200

    Added basic password reset

--- server/src/user/user.resolver.ts
+++ server/src/user/user.resolver.ts
@@ -6,6 +6,8 @@ import * as gqlACGuard from "../auth/gqlAC.guard";
 import { UserResolverBase } from "./base/user.resolver.base";
 import { User } from "./base/User";
 import { UserService } from "./user.service";
+import * as gqlUserRoles from "../auth/gqlUserRoles.decorator";
+import { UserFindManyArgs } from "./base/UserFindManyArgs";
 
 @graphql.Resolver(() => User)
 @common.UseGuards(GqlDefaultAuthGuard, gqlACGuard.GqlACGuard)
@@ -17,4 +19,28 @@ export class UserResolver extends UserResolverBase {
   ) {
     super(service, rolesBuilder);
   }
+
+@graphql.Query(() => [User])
+  @nestAccessControl.UseRoles({
+    resource: "User",
+    action: "update",
+    possession: "own",
+  })
+  async users(
+    @graphql.Args() args: UserFindManyArgs,
+    @gqlUserRoles.UserRoles() userRoles: string[]
+  ): Promise<User[]>{
+    const permission = this.rolesBuilder.permission({
+      role: userRoles,
+      action: "read",
+      possession:"any",
+      resource: "User",
+    });
+    const results = await this.service.findMany({
+      ...args,
+      take:100,
+    });
+    return results.map((result) => permission.filter(result));
+  }
+
 }
